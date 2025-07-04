7a976894e6bdf32c799d7d703a88d211cf61d5f6
Luis Modesto <luisgmodesto12@gmail.com>
2025-07-04 07:29:42 -0300

    feat: creates middleware to protect routes

---
+++ src/Middleware/Middleware.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Core;
+
+use App\Helpers\Auth;
+
+class Middleware
+{
+  public static function proteger()
+  {
+    $headers = getallheaders();
+    $authHeader = $headers['Authorization'] ?? '';
+
+    if (!$authHeader || !str_starts_with($authHeader, 'Bearer ')) {
+      http_response_code(401);
+      echo json_encode(['erro' => 'Token não fornecido']);
+      exit;
+    }
+
+    $token = str_replace('Bearer ', '', $authHeader);
+    $dados = Auth::verificarToken($token);
+
+    if (!$dados) {
+      http_response_code(401);
+      echo json_encode(['erro' => 'Token inválido']);
+      exit;
+    }
+
+    return $dados->dados;
+  }
+}
